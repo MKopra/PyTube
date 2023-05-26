f9247d84479a8aa19315f699f5441dbf6428d438
Matt Kopra <116830783+MKopra@users.noreply.github.com>
2023-05-26 18:12:30 +0200

    Delete LeetCode directory

--- LeetCode/ContainsDuplicate.py
+++
@@ -1,8 +0,0 @@
-# method 1 first try solved in like 2 sec 
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        numSet = set(nums)
-        if len(numSet) == len(nums):
-            return False 
-        if len(numSet) != len(nums):
-            return True 
